1504f2d2c17502d9010f6147902633dffa050c15
Esteban Mathia <esteban.mathia@supinfo.com>
2023-10-24 15:50:27 +0200

    debug

--- src/services/alerte.service.ts
+++ src/services/alerte.service.ts
@@ -47,13 +47,14 @@ export function alertGlobal(allScan: ResultScan[][], alert: GlobalConfigAlert): 
 }
 
 export function alertFromGlobal(alert: GlobalConfigAlert, compteError: number[], allScan: ResultScan[][]) {
-    allScan = allScan.map(scan => scan.filter(value => value.error.length>0))
+    allScan = allScan.map(scan => scan.filter(value => value.error.length>0));
     alert.type.forEach((type) => {
         switch(type){
             case AlertEnum.LOG:
                 alertLogGlobal(alert, compteError, allScan);
                 break;
             case AlertEnum.EMAIL:
+                logger.info("alert email");
                 alertEmailGlobal(alert, compteError, allScan);
                 break;
             case AlertEnum.SMS:
@@ -100,7 +101,7 @@ export function alertEmailGlobal(alert: GlobalConfigAlert, compteError: number[]
     logger.debug("alert email");
     alert.to.forEach((email_to) => {
         if(!email_to.includes("@")) return;
-        logger.debug("send email to:"+email_to);
+        logger.info("send email to:"+email_to);
         let render_table = renderTableAllScan(allScan.map(scan => scan.filter(value => value.error.length>0)));
         let mail = Emails.GlobalAlert(email_to, compteError, render_table, alert);
         SendMailWithAttachment(mail, email_to, "Kexa - Global Alert - "+(alert.name??"Uname"), compteRender(allScan));
