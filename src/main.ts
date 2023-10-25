25aea5d223b0c32494769de8c6431e053c08b631
Esteban Mathia <esteban.mathia@supinfo.com>
2023-10-25 12:53:49 +0200

    break: release

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,3 @@
-//import * as core from '@actions/core'
-//import { wait } from './wait'
-//
-///**
-// * The main function for the action.
-// * @returns {Promise<void>} Resolves when the action is complete.
-// */
-//export async function run(): Promise<void> {
-//  try {
-//    const ms: string = core.getInput('milliseconds')
-//
-//    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-//    core.debug(`Waiting ${ms} milliseconds ...`)
-//
-//    // Log the current timestamp, wait, then log the new timestamp
-//    core.debug(new Date().toTimeString())
-//    await wait(parseInt(ms, 10))
-//    core.debug(new Date().toTimeString())
-//
-//    // Set outputs for other workflow steps to use
-//    core.setOutput('time', new Date().toTimeString())
-//  } catch (error) {
-//    // Fail the workflow run if an error occurs
-//    if (error instanceof Error) core.setFailed(error.message)
-//  }
-//}
-
 import { checkRules, gatheringRules } from "./services/analyse.service";
 import { alertGlobal } from "./services/alerte.service";
 import { AsciiArtText, talkAboutOtherProject} from "./services/display.service";
@@ -41,7 +14,6 @@ require('dotenv').config();
 
 export async function main() {
     core.addPath('./config');
-    //core.addPath('./rules');
     let customRules = await getEnvVar("MYOWNRULES");
     if(customRules != "NO"){
         core.addPath(customRules);
@@ -86,7 +58,6 @@ export async function main() {
     logger.info("_______________________________________-= End Kexa scan =-_________________________________________");
     logger.info("___________________________________________________________________________________________________");
     talkAboutOtherProject();
-    //logger.debug(await getEnvVar("test"));
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
