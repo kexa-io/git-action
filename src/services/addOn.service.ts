14e4ef36b2272f55b2860d75ea440e4ef5320848
aeppling <adrien.eppling@epitech.eu>
2023-11-23 14:49:58 +0100

    path

--- src/services/addOn.service.ts
+++ src/services/addOn.service.ts
@@ -4,7 +4,7 @@ import { writeStringToJsonFile } from "../helpers/files"
 const configuration = require('config');
 
 const mainFolder = 'Kexa';
-const serviceAddOnPath = '/services/addOn';
+const serviceAddOnPath = './addOn';
 const fs = require('fs');
 
 import { getNewLogger} from "./logger.service";
