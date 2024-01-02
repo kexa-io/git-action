f9b16e3e5b9b32d23f35ef5c364280fd7993b63f
Esteban Mathia <esteban.mathia@supinfo.com>
2024-01-02 14:29:38 +0100

    update Azure Gathering with old item and rework get

--- src/models/azure/resource.models.ts
+++ src/models/azure/resource.models.ts
@@ -3,18 +3,21 @@ import { NetworkSecurityGroup, VirtualNetwork } from "@azure/arm-network";
 import { ResourceGroup } from "@azure/arm-resources";
 
 export interface AzureResources {
-    vm: Array<NetworkSecurityGroup>|null;
-    rg: Array<VirtualMachine>|null;
-    disk: Array<Disk>|null;
-    nsg: Array<ResourceGroup>|null;
-    virtualNetwork: Array<VirtualNetwork>|null;
-    networkInterfaces: Array<NetworkSecurityGroup>|null;
-    namespaces: Array<any>|null;
-    pods: Array<any>|null;
-    aks: Array<any>|null;
-    ip: Array<any>|null;
-    mlWorkspaces: Array<any>|null;
-    mlJobs: Array<any>|null;
-    mlComputes: Array<any>|null;
-    mlSchedules: Array<any>|null;
+    'KexaAzure.vm': Array<NetworkSecurityGroup>|null;
+    'KexaAzure.mlWorkspaces': Array<any>|null;
+    'KexaAzure.mlJobs': Array<any>|null;
+    'KexaAzure.mlComputes': Array<any>|null;
+    'KexaAzure.mlSchedules': Array<any>|null;
+    'KexaAzure.storage': Array<any>|null;
+    'KexaAzure.blob': Array<any>|null;
 }
+
+export const stringKeys: Array<String> = [
+    'KexaAzure.vm',
+    'KexaAzure.mlWorkspaces',
+    'KexaAzure.mlJobs',
+    'KexaAzure.mlComputes',
+    'KexaAzure.mlSchedules',
+    'KexaAzure.storage',
+    'KexaAzure.blob',
+];
