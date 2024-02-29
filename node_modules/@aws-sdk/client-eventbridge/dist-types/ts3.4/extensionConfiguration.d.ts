488d78d6beaeaeb5c7bf25763830f78f74540d79
Esteban Mathia <esteban.mathia@supinfo.com>
2024-02-29 17:40:22 +0100

    continue #8

---
+++ node_modules/@aws-sdk/client-eventbridge/dist-types/ts3.4/extensionConfiguration.d.ts
@@ -0,0 +1,7 @@
+import { AwsRegionExtensionConfiguration } from "@aws-sdk/types";
+import { HttpHandlerExtensionConfiguration } from "@smithy/protocol-http";
+import { DefaultExtensionConfiguration } from "@smithy/types";
+export interface EventBridgeExtensionConfiguration
+  extends HttpHandlerExtensionConfiguration,
+    DefaultExtensionConfiguration,
+    AwsRegionExtensionConfiguration {}
