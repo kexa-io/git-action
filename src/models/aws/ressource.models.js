7d10131878675b0287dabcbaf55298cc2f6409cb
Esteban Mathia <esteban.mathia@supinfo.com>
2024-03-01 13:10:55 +0100

    updating pkg

---
+++ src/models/aws/ressource.models.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.stringKeys = void 0;
+exports.stringKeys = [
+    'KexaAwsCustoms.tagsValueListing',
+    'KexaAwsCustoms.resourcesTags'
+];
---
+++ src/models/azure/resource.models.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.stringKeys = void 0;
+exports.stringKeys = [
+    'KexaAzure.vm',
+    'KexaAzure.mlWorkspaces',
+    'KexaAzure.mlJobs',
+    'KexaAzure.mlComputes',
+    'KexaAzure.mlSchedules',
+    'KexaAzure.storage',
+    'KexaAzure.blob',
+];
