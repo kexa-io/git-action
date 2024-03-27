70184fd0b777b1a2fa3c0c14dd38abe809712e30
Esteban Mathia <esteban.mathia@supinfo.com>
2024-03-27 17:53:49 +0100

    remove nodes #4

--- node_modules/@aws-sdk/client-cloudfront/dist-es/pagination/ListCloudFrontOriginAccessIdentitiesPaginator.js
+++
@@ -1,4 +0,0 @@
-import { createPaginator } from "@smithy/core";
-import { CloudFrontClient } from "../CloudFrontClient";
-import { ListCloudFrontOriginAccessIdentitiesCommand, } from "../commands/ListCloudFrontOriginAccessIdentitiesCommand";
-export const paginateListCloudFrontOriginAccessIdentities = createPaginator(CloudFrontClient, ListCloudFrontOriginAccessIdentitiesCommand, "Marker", "CloudFrontOriginAccessIdentityList.NextMarker", "MaxItems");
