f78abc615af228abab3c727acfdeec156048c271
Esteban Mathia <esteban.mathia@supinfo.com>
2024-03-27 19:59:29 +0100

    node delete#20

--- node_modules/@aws-sdk/client-textract/dist-types/ts3.4/pagination/Interfaces.d.ts
+++
@@ -1,6 +0,0 @@
-import { PaginationConfiguration } from "@smithy/types";
-import { TextractClient } from "../TextractClient";
-export interface TextractPaginationConfiguration
-  extends PaginationConfiguration {
-  client: TextractClient;
-}
--- node_modules/@aws-sdk/client-transcribe/dist-es/pagination/ListVocabularyFiltersPaginator.js
+++
@@ -1,4 +0,0 @@
-import { createPaginator } from "@smithy/core";
-import { ListVocabularyFiltersCommand, } from "../commands/ListVocabularyFiltersCommand";
-import { TranscribeClient } from "../TranscribeClient";
-export const paginateListVocabularyFilters = createPaginator(TranscribeClient, ListVocabularyFiltersCommand, "NextToken", "NextToken", "MaxResults");
