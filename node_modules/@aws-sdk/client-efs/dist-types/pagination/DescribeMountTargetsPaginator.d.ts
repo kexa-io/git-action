498f36ff020edfdf6684dd60075abd01f6abaa2d
Esteban Mathia <esteban.mathia@supinfo.com>
2024-04-09 18:57:47 +0200

    #5

--- node_modules/@aws-sdk/client-efs/dist-types/pagination/DescribeMountTargetsPaginator.d.ts
+++
@@ -1,7 +0,0 @@
-import { Paginator } from "@smithy/types";
-import { DescribeMountTargetsCommandInput, DescribeMountTargetsCommandOutput } from "../commands/DescribeMountTargetsCommand";
-import { EFSPaginationConfiguration } from "./Interfaces";
-/**
- * @public
- */
-export declare const paginateDescribeMountTargets: (config: EFSPaginationConfiguration, input: DescribeMountTargetsCommandInput, ...rest: any[]) => Paginator<DescribeMountTargetsCommandOutput>;
